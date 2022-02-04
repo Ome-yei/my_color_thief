160b24aabc7854da50eb5921f3fe689d89e56aeb
Carlos Placencia <carlosplacencia2012@gmail.com>
2022-02-04 00:06:05 -0600

    added color theme to urlInput

--- src/components/PasteUrl.js
+++ src/components/PasteUrl.js
@@ -1,24 +1,42 @@
+import { useContext } from 'react';
+import { ImgDominantColor } from './themeContext';
 import styled from 'styled-components';
+import helpers from './helpers';
 
 
 export default function PasteUrl({ handleInputChange, handleSubmit, inputUrl }) {
+
+  const value = useContext(ImgDominantColor);
+
   return (
     <PasteUrlForm onSubmit={handleSubmit}>
-      <PasteUrlInput type='text' value={inputUrl} onChange={handleInputChange} />
-      <PasteUrlButton>Get Colors</PasteUrlButton>
+      <PasteUrlInput
+        type='text'
+        value={inputUrl}
+        onChange={handleInputChange}
+        darkerColor={helpers.darkenColor(value)}
+      />
+      <PasteUrlButton
+        brightenColor={helpers.brightenColor(value)}
+        darkerColor={helpers.darkenColor(value)}
+      >
+        Get Colors
+      </PasteUrlButton>
     </PasteUrlForm>
   )
 }
 
 const PasteUrlInput = styled.input`
-  border: 1px solid #e1e1e1;
+  border: 3px solid ${props => props.darkerColor};
   padding: .2em 1em;
   border-radius: .375em 0 0 .375em;
   width: 100%
 `;
 
 const PasteUrlButton = styled.button`
-border: 1px solid #e1e1e1;
+  border: 3px solid ${props => props.darkerColor};
+  background-color:  ${props => props.brightenColor};
+  color: ${props => props.darkerColor};
   margin-left: -1px;
   border-radius: 0 6px 6px 0;
   padding: .2em .75em;
