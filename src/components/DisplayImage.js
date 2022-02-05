ac4fe3f35999d626dce723a89527892770842fb1
Carlos Placencia <carlosplacencia2012@gmail.com>
2022-02-05 15:50:40 -0600

    added shadow on image container

--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -27,6 +27,7 @@ const ImageContainer = styled.div`
 // border: 1px solid red;
 margin: 0 auto;
 max-width: 500px;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `;
 
 const Displaytheimage = styled.img`
